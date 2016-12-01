bb954b0f95dc65838ab5958d11781bb20412bea4
KryvunRoman <kryvun.roman@gmail.com>
2016-12-02 01:56:02 +0200

    Show all gifs list.

---
+++ main.js
@@ -0,0 +1,153 @@
+const LinksList = [
+    'http://www.clipartkid.com/images/92/christmas-deer-JTyoge-clipart.gif',
+    'http://www.clipartkid.com/images/551/advocates-for-highway-and-auto-safety-leo-law-enforcement-officer-not-fD3KGn-clipart.gif',
+    'http://www.clipartkid.com/images/551/11-plain-christmas-trees-free-cliparts-that-you-can-download-to-you-WTXKzt-clipart.gif',
+    'http://gif-animation.leovacity.be/images/jumping-monkey-gif-animation.gif',
+    'http://www.citymouse.de/weihnachten/saubernickolaus.gif',
+    'http://cliparts.co/cliparts/8cA/69X/8cA69XKRi.gif',
+    'https://ponyach.ru/b/src/145495427883.gif',
+    'https://fc01.deviantart.net/fs71/f/2013/106/c/7/blinktest_by_luga12345-d61xjpn.gif',
+    'http://vignette3.wikia.nocookie.net/disney/images/9/9a/Sorcerermickey1.png/revision/latest?cb=20160509204352&path-prefix=pt-br',
+    'https://regmedia.co.uk/2014/01/23/nyancoin_logo.gif?x=1200&y=794',
+    'http://1.viki.io/d/1863c/8b75dc48c9.gif',
+    'http://i.imgur.com/nNocR.gif',
+    'https://s-media-cache-ak0.pinimg.com/originals/01/58/50/01585018aba0f4245e9eb495f9257326.jpg',
+    'http://i.imgur.com/WKt6HuI.gif',
+    'http://i3.kym-cdn.com/photos/images/original/000/803/459/8ab.gif',
+    'https://derpicdn.net/img/2014/4/5/593662/medium.gif',
+    'http://redheadedmule.com/wp-content/uploads/2012/11/pinkie_pie_and_fluttershy_drop_it_like_it__s_hot_by_tomdantherock.gif',
+    'http://fast1.onesite.com/capcom-unity.com/user/cheerfuljochan/random_pics_not_made_by_me/9819e3de75ed0efc5c9d3f684efde784.gif?v=70692',
+    'http://i264.photobucket.com/albums/ii178/paaao16/snorlax.gif',
+    'http://25.media.tumblr.com/8512c9a9e1db2d86903bcbffe4f1dd1a/tumblr_mwnl78MiIC1qedkdeo1_250.gif',
+    'http://orig10.deviantart.net/7574/f/2013/332/d/5/ross_vs_danny_by_frario-d6vyo05.gif',
+    'https://cdn2.scratch.mit.edu/get_image/user/2397916_90x90.png?v=1384569291.0',
+    'http://trucosclubpenguin.net/wp-content/uploads/2013/02/Scrap-Proximos-villanos-en-Club-Penguin-2013.gif',
+    'https://www.pokebip.com/pages/animation/dossier/Pikachul.png',
+    'http://25.media.tumblr.com/18fdf40215f34611beb6ca39c879ce91/tumblr_mo1or3QdiJ1s9gisfo1_400.gif',
+    'http://twominutenoodles.com/content/1-blog/69-MexiNinja/69-MexiNinja.gif',
+    'http://orig09.deviantart.net/b09f/f/2015/230/e/b/2dancing_pikas_2_by_bestary-d967kcu.gif',
+    'http://i734.photobucket.com/albums/ww350/harry7bagus/Sprites/subzero-mk3-hado.gif',
+    'https://s-media-cache-ak0.pinimg.com/originals/0c/ed/a3/0ceda3d2ab317b8326149257d4639db4.gif',
+    'https://octodex.github.com/images/daftpunktocat-thomas.gif',
+    'http://www.emoticones-avenue.com/emoticones/comics_batman_logo_3d_01.gif',
+    'http://media-s3-us-east-1.ceros.com/mashable/images/2015/05/08/80f3c8a04cdc3af326f8036d53b69536/bb-8-2.gif',
+    'http://vignette2.wikia.nocookie.net/ecole-oraliste-eslvocabulary/images/e/e7/Raincloud_sun1.gif/revision/latest?cb=20150206220640',
+    'http://i47.tinypic.com/rssylu.gif',
+    'http://images.clipartpanda.com/leap-clipart-jcxExGbji.gif',
+    'https://api.dubtrack.fm/user/564f74fefd925a8800163dfd/image',
+    'http://www.mascotcreatortoolkit.com/wp-content/uploads/2016/05/FitnessMale_exercise_leg_1.gif',
+    'https://lh5.googleusercontent.com/proxy/v2RIlWL-8y8GMus4vySJtJ0GAG7FRH_C5_aop0ly5tdMNcjwJXr43qnK34Fau0tEcx7WqLKypk8JXbCuI09TEX2wBBbSFSDA-I_wxg=s0-d',
+    'http://mascotcreatortoolkit.com/img/SaxPlayer_play-saxophone.gif',
+    'https://www.kasandbox.org/programming-images/misc/cat-walk.gif',
+    'http://www.piubici.org/wp-content/uploads/2016/01/tumblr_static_tumblr_static_filename_640.gif',
+    'http://www.mascotdesigngallery.com/wp/wp-content/uploads/2013/11/wombat_by_arc_en_ciel_chan-d4tk2xk.gif',
+    'https://s3.amazonaws.com/media-p.slid.es/uploads/alessandroannini/images/868913/daftpunktocat-guy.gif',
+    'https://derpicdn.net/img/view/2014/2/12/549799__safe_solo_rarity_animated_vector_artist-colon-masem_-dot-svg+available_spoiler-colon-s4e13.gif',
+    'https://media.giphy.com/media/5JWoFiJOVjP56/giphy.gif',
+    'https://s-media-cache-ak0.pinimg.com/originals/3d/37/b0/3d37b00937f6043138c6e0753ffc1074.gif',
+    'http://d21c.com/swiftdreams/Halloween/AnimatedGhost1.gif',
+    'https://d44ytnim3cfy5.cloudfront.net/assets/3236944/original/rabbit.gif?1300689648',
+    'http://www.clipartkid.com/images/660/16-chili-pepper-border-free-cliparts-that-you-can-download-to-you-7DRI9z-clipart.gif',
+    'http://orig12.deviantart.net/0274/f/2012/107/3/5/_acchi_kocchi__ears__how_do_they_work__by_tach_ko-d4widvx.gif',
+    'http://myfirstchat.com/myfirstworld/aug14/21956PIZZAEATING.gif',
+    'http://4.bp.blogspot.com/-2Ue-dQkSzmI/U8mYEb_fhYI/AAAAAAABfLc/lW3msj0w8Uk/s1600/144.gif',
+    'https://67.media.tumblr.com/4966b433e8782a44030e6c51dfba3179/tumblr_inline_n61n2ppTj01qbolbn.gif',
+    'https://33.media.tumblr.com/f61f22fe6072433efba3be6cb39bd262/tumblr_n2pqjcC1kT1s1rd1xo1_500.gif',
+    'http://24.media.tumblr.com/fe2ccff09f322fdc3d6220639f2fb273/tumblr_mfzbzoGzCh1rgpyeqo1_400.gif',
+    'https://s-media-cache-ak0.pinimg.com/originals/27/44/fc/2744fc241aa1218f9f0d771822a99db9.gif',
+    'http://rs881.pbsrc.com/albums/ac19/quasisonic/zoidberg_dance_animate.gif~c200',
+    'http://media0.giphy.com/media/llgy9Ywc5CvwA/giphy.gif',
+    'http://rs708.pbsrc.com/albums/ww89/brendon_01/Cartoons/gir-7.gif~c200',
+    'http://static.tumblr.com/6bffaeb81d90b73bcc2e5d930bb47c72/ckpit8w/z6Wn5qae6/tumblr_static_7lwcchbt75s04004ogkc0ckc8.gif',
+    'https://media.giphy.com/media/9g1ju7QzhLmgg/giphy.gif',
+    'https://66.media.tumblr.com/c11e96024cae1badc4b0d39ed78ce052/tumblr_nfd7kf9QQ91ronhn5o1_400.gif',
+    'http://giffiles.alphacoders.com/720/7206.gif',
+    'https://s-media-cache-ak0.pinimg.com/originals/02/57/69/0257695066c4b8a38543aabfb0679a4e.gif',
+    'http://www.clipmass.com/userthumb/49555.gif',
+    'http://static3.wikia.nocookie.net/__cb20120708024057/victorious/images/9/9e/Dancing_Troll_gif.gif',
+    'http://i1207.photobucket.com/albums/bb466/MikairuDaidouji/toothychibisis.gif',
+    'https://media.giphy.com/media/bXguSfnEakGFG/giphy.gif',
+    'https://s-media-cache-ak0.pinimg.com/originals/f5/b6/88/f5b688276139640aee6dbb4de8d8afd5.gif',
+    'http://static.tumblr.com/01e7b577cebeba1cc3f3bdcb0e8d54a3/5m1sleu/2G4ogcf5i/tumblr_static_9t2ik4d987ocwskssg0s8oc8c.gif',
+    'http://media.giphy.com/media/MmmGhAnxohVew/giphy.gif',
+    'http://drakinan.weebly.com/uploads/1/3/4/1/13418183/4623978_orig.gif',
+    'http://www.gif-anime.org/im/gif/animal/hamster/hamster31.gif',
+    'http://31.media.tumblr.com/890c0a0e72d0900866092db696fc9661/tumblr_mu8gbqF58k1rqn54io1_500.gif',
+    'https://s-media-cache-ak0.pinimg.com/originals/78/93/5f/78935f763f414d8ace619a2e4f36919c.gif',
+    'http://www.imgnly.com/wp-content/uploads/2014/01/Bunny-dance.gif',
+    'https://static4.fjcdn.com/thumbnails/comments/What+got+me+was+how+it+was+unexpected+_a115920c71ff5009f31d062074e68a69.gif',
+    'http://hbvideogames.com/assets/spinner/tumblr_m3u9txydOt1rptf33o1_500-6a74d4c40091d9f62aa8b88b9901cf59.gif',
+    'http://2.bp.blogspot.com/-G58ppJmxvX0/VHadUJBR0GI/AAAAAAAAFIY/WRUu_yFybck/s1600/tux-animado.gif',
+    'http://www.bloomberg.com/businessweek/85ideas/images/open_source.gif',
+    'https://linux-bibel-online.org/bau.gif',
+    'http://3.bp.blogspot.com/-mcasnuqHGLY/Vj4afCOK-MI/AAAAAAAABZk/9b9cWfsp5Jw/s752/animated-computer-image-0004.gif',
+    'https://app-stickers.s3.amazonaws.com/838500730/819630598/c5e5fe8d3f1d611997627f390e60ae13-2.gif',
+    'https://app-stickers.s3.amazonaws.com/838500730/819630598/584689f5bd2525445199aeb39acb2334-4.gif',
+    'https://app-stickers.s3.amazonaws.com/838500730/819630598/908a214e1e5803e21cf1f14dd252147f-5.gif',
+    'https://app-stickers.s3.amazonaws.com/838500730/819630598/abbe6733ec83a938a6b80f97d382c375-6.gif',
+    'https://familyguyaddicts.files.wordpress.com/2014/04/peter-chris-dualaction.gif',
+    'http://vignette2.wikia.nocookie.net/familyguyfanon/images/0/08/Vinny_wagging_his_tail.gif/revision/latest?cb=20140609210957',
+    'http://orig14.deviantart.net/1746/f/2014/326/b/c/stewie_and_brian_march_for_xmas_ani_by_mighty355-d879yn3.gif',
+    'https://app-stickers.s3.amazonaws.com/838500730/819630598/cbc78b663f0728943c916cc44421e294-3.gif',
+    'http://zealicon.in/zeal/images/popeye.gif',
+    'http://puppsfreestuff.com/files/Pumba7f.gif',
+    'http://m.gifmania.co.kr/Gifs-Walt-Disney/Gifs-Disney-Characters/Gifs-Donald-Duck/Donald/Donald-33975.gif',
+    'http://tstonews.com/imgs/4_4_0r5_PattyAndSelma/anim/patty_take_a_smoke_break.gif',
+    'http://orig13.deviantart.net/3504/f/2015/112/2/2/cockroach_gif_by_evanest-d8qntjq.gif',
+    'http://orig00.deviantart.net/0d77/f/2013/081/d/8/patty_cake_with_fluttershy_____by_mlpinkiepieofficial-d5yx0jj.gif',
+    'http://rs1028.pbsrc.com/albums/y349/kerstinabersfelder21/received_393237594203800_zpsvoxwvzyw.gif~c200',
+    'https://lh3.googleusercontent.com/-Vi0b5-pEAk8/AAAAAAAAAAI/AAAAAAAAAAA/OPtOxVg4K9I/photo.jpg',
+    'http://www.windows-addict.com/despicable/min.gif',
+    'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/iblock/8cf/8cfebbe2a5e453f311547ac17f77c531.png?1448436950183991'
+];
+
+const Header = React.createClass({
+    render() {
+        return (
+            <h1 className="page__header">React Gifs</h1>
+        );
+    }
+});
+
+const GifsList = React.createClass({
+    render() {
+        return (
+            <div>
+                {
+                    LinksList.map((gifItem, index) =>
+                        <img
+                            key={index}
+                            src={gifItem}
+                            style={{height: 90, width: 'auto'}}
+                        />
+                    )
+                }
+            </div>
+        );
+    }
+});
+
+const RangomImage = React.createClass({
+    getRandomGif() {
+        const randomLink = LinksList[Math.floor(Math.random() * LinksList.length)];
+        return randomLink;
+    },
+
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <img alt="" src={this.getRandomGif()} style={{height: 120}}/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <div>
+        <Header />
+        <RangomImage/>
+        <GifsList/>
+    </div>
+    ,
+    document.getElementById('root')
+);
